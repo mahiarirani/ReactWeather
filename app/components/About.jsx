b33a65ff6007da1d77847a6ede35d45c90d00aee
Mahyar Irani <mahiarirani@yahoo.com>
2017-11-29 09:44:01 +0330

    Style about

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,5 +1,15 @@
 let React = require('react');
 
-let About = () => <div><h3>About</h3><p>Welcome to the about page.</p></div>;
+let About = () => (
+    <div>
+        <h1 className="text-center">About</h1>
+        <p>This is a weather application built on React using the OpenWeatherMap.</p>
+        <p>Here are links to the tools:</p>
+        <ul>
+            <li><a href="https://github.com/facebook/react">React</a> - Javascript Framework</li>
+            <li><a href="http://openweathermap.org/">OpenWeatherMap</a> - Weather API</li>
+        </ul>
+    </div>
+);
 
 module.exports = About;
